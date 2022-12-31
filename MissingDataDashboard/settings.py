57e22a9476c65b3a5928cd7fefdadf23b0c2574a
KPaulina <41211513+KPaulina@users.noreply.github.com>
2022-12-31 12:48:47 +0100

    fix: security problem settings.py

--- MissingDataDashboard/settings.py
+++ MissingDataDashboard/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import dotenv
 from pathlib import Path
 import os
 
+dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hhq$)dx0_rl_=!h*f)us1-^0n_zd)ts$mktxg#th%sg(a!&_g^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
